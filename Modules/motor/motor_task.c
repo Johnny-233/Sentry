8923fce6794bbd0b59c4bebd0562f10e4d3dfd1e
Johnny233 <y1960857741@163.com>
2026-07-17 22:54:53 +0800

    哨兵导航通信完成

---
+++ Modules/motor/motor_task.c
@@ -0,0 +1,12 @@
+#include "motor_task.h"
+#include "dji_motor.h"
+#include "mi_motor.h"
+void MotorControlTask()
+{
+    static uint8_t cnt = 0; 
+    if(cnt%5==0) //200hz
+    DJIMotorControl();
+    if(cnt%10==0) //100hz
+    MiMotorControl();
+
+}
